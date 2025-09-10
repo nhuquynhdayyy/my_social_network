fdf4424927be3b537ea9e4164d4bc8e151ae91e4
nhuquynhdayyy <102230099@sv1.dut.udn.vn>
2025-09-10 15:47:15 +0700

    remove context_processors

--- core/context_processors.py
+++
@@ -1,8 +0,0 @@
-# core/context_processors.py
-from notifications.models import Notification
-
-def unread_notifications_count(request):
-    if request.user.is_authenticated:
-        count = Notification.objects.filter(recipient=request.user, is_read=False).count()
-        return {'unread_notifications_count': count}
-    return {}
